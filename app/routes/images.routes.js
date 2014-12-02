aeec64d29868431990b700ba740bafbec3c745e3
Daniel <dbeaudreau@solinkcorp.com>
2014-12-01 21:11:17 -0500

    Route for saving images, refactoring for generateImageUrlExtension method

--- app/routes/images.routes.js
+++ app/routes/images.routes.js
@@ -1,8 +1,9 @@
 var Image         	= require('../models/image');
+var fs 				= require('fs');
 
 module.exports = function(router) {
 
-    router.get('/getImageInfo', function(req,res){
+    router.get('/getImageInfo', function(req, res){
       var imageQuery = Image.find({username : req.param('id')});
       imageQuery.exec(function(err, imageInfo){
         if(err){
@@ -11,11 +12,24 @@ module.exports = function(router) {
         res.json(imageInfo);
       });
     });
-
+	
+	router.post('/postImage', function(req, res){
+		var filePath  = '/images/' + generateImageUrlExtension();
+		fs.rename(req.files.photo.path, filePath, 	function(error) {
+            if(error) {
+				return;
+			}
+            res.send({
+				status: success;
+            });
+		});
+	});
+	
+	
 	function generateImageUrlExtension() {
 		var imageUrl = "";
-		for(int i = 0; i < 8; i++) {
-			imageUrl += String.fromCharCode(0x0041 + Math.random() * 26);
+		for(var i = 0; i < 100; i++) {
+			imageUrl += String.fromCharCode(0x0040 + Math.random() * (0x005B - 0x0041)+1);
 		}
 		return imageUrl;
 	}
